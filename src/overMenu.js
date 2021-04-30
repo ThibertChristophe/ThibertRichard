15a3b45e182c7dec7b12dc19f7af40d9d5461f0a
ThibertChristophe <c.thibert89@gmail.com>
2021-04-30 22:14:01 +0200

    Hidden menu index in JS
    Cache le menu de l'index quand il touche la div de contenu

--- src/overMenu.js
+++ src/overMenu.js
@@ -21,3 +21,19 @@ menu.forEach((elem) => {
     });
 });
 
+
+/**
+ * Fait disparaitre le menu header quand il touche la div de contenu
+ */
+let menuT = document.getElementById('menuTop');
+window.onscroll = function() {
+    const containt = document.getElementsByClassName('contentIndex');
+    let y = containt[0].getBoundingClientRect().y;
+    let yMenu = 57;
+    if (yMenu > y) {
+        menuT.style.top = "-58px";
+    } else{
+    menuT.style.top = "0";
+    }
+}
+
