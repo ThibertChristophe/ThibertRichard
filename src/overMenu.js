b9633d6b3d3d8d813e66a730acbe0c775539a016
ThibertChristophe <c.thibert89@gmail.com>
2021-04-30 22:16:15 +0200

    Update overMenu.js

--- src/overMenu.js
+++ src/overMenu.js
@@ -1,27 +1,5 @@
 "use strict"
 
-/* Script pour faire le zoom des photos de la page Exposition (expo.html) */
-
-/* Liste des photos de la page */
-const menu = document.querySelectorAll('div.menuTop.animate__animated.animate__fadeInDown > nav > li >a');
-
-const barre = document.getElementsByClassName('lineOver');
-
-console.log(menu);
-/* Ajout d'event de click sur la liste des photos */
-/* On rendra visible la zone de zoom avec la photo concernée quand on click sur une de nos photos */
-menu.forEach((elem) => {
-    elem.addEventListener('mouseover', (e) => {
-        /* Recup l'image source */
-       console.log("IN");
-    });
-    elem.addEventListener('mouseout', (e) => {
-        /* Recup l'image source */
-       console.log("OUT");
-    });
-});
-
-
 /**
  * Fait disparaitre le menu header quand il touche la div de contenu
  */
