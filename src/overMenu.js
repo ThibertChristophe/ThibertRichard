b5fd017dafd04f07080753a77036c3478e04e8f1
ThibertChristophe <c.thibert89@gmail.com>
2021-05-02 22:41:02 +0200

    Menu hide & scroll top

--- src/overMenu.js
+++ src/overMenu.js
@@ -5,14 +5,19 @@
  */
 let menuT = document.getElementById('menuTop');
 const yMenu = 57;
+let fleche = document.getElementById('fleche');
+fleche.addEventListener('click', () => {
+    window.scrollTo(0,0);
+})
 
 window.addEventListener('scroll', () => {
-    const containt = document.getElementsByClassName('contentIndex');
+    const containt = document.getElementsByClassName('containt');
     let y = containt[0].getBoundingClientRect().y;
     if (yMenu > y) {
         menuT.style.top = "-58px";
+      //  fleche.style.visibility = 'visible';
     } else{
         menuT.style.top = "0";
+       // fleche.style.visibility = 'hidden';
     }
 });
-
